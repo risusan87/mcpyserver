706cede0016ccfc96f370e7732cbd98d9efacbf2
risusan87 <risusan8795@gmail.com>
2024-12-25 08:52:56 -0500

    Follow conventions

--- core/console.py
+++ core/console.py
@@ -5,12 +5,12 @@ import threading
 from logger import logger
 from networking import stop_server
 
-def command_listener():
+def _command_listener():
     while True:
         command = input()
-        handle(command)
+        _handle(command)
 
-def handle(command):
+def _handle(command):
     if command == 'stop':
         stop_server()
         os._exit(0)
@@ -18,5 +18,5 @@ def handle(command):
         logger.info(f'Unknown command: {command}', log_thread=False)    
 
 def start_command_listener():
-    command_listener_thread = threading.Thread(target=command_listener)
+    command_listener_thread = threading.Thread(target=_command_listener)
     command_listener_thread.start()
